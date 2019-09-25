954bb62d866dd5826ce496fff486c456381ceed7
MSTC Signal Processing and Machine Learning for Big Data <master.stc.ssr@gmail.com>
2019-09-25 19:10:19 +0200

    Update WORKING_LinearRegression_Advertising.R

--- WORKING_LinearRegression_Advertising.R
+++ WORKING_LinearRegression_Advertising.R
@@ -39,6 +39,65 @@ pairs(~ TV + Radio + News.paper + Sales, Advertising)
 # add marginal histograms
 #ggExtra::ggMarginal(p, type = "histogram")
 
+
+# Multiple Linear Regression
+# lm.fit=lm(Sales~TV+Radio+Newspaper,data=Advertising)
+
+# ~.-X = all variables but X (as X is not a variable  . it means use ALL !)
+
+lm.fit=lm(Sales~.-X,data=Advertising)
+summary(lm.fit)
+
+
+##### SOME FURTHER TOPICS to study ######
+#########################################
+
+#Go back to a Simple Linear Regression: Sales ~ TV
+lm.fit=lm(Sales~TV)
+
+
+# R^2 and correlation with predicted values Corr(Y,Y_hat)
+summary(lm.fit)
+Pred_Sales=predict(lm.fit,data.frame(TV),interval="prediction")
+cor(Sales,Pred_Sales)^2
+
+# As it is a simple prediction Check it is also Cor(X,Y) 
+cor(Sales,TV)^2
+
+# Other metrics
+plot(abs(Sales-Pred_Sales[,1]),xlab="Sales",type='p',pch=12,col='blue')
+
+MAE=sum(abs(Sales-Pred_Sales[,1]))/length(Sales)
+
+
+# CONFIDENCE and PREDICTION intervals
+# 95% confidence interval associated with each TV value
+predict(lm.fit,data.frame(TV=c(10,100,200)),interval="confidence")
+
+# prediction interval for predicted Sales
+predict(lm.fit,data.frame(TV=c(10,100,200)),interval="prediction")
+
+
+
+# INTERACTION TERMS
+lm.fit=lm(Sales~TV*Radio,data=Advertising)
+summary(lm.fit)
+
+# NON-LINEARITIES
+# Residual plots are a useful graphical tool for identifying non-linearity 
+# Plotting residualts
+plot(predict(lm.fit),residuals(lm.fit),xlab='Fitted values',ylab='Residuals',main='Residual Plot for Advertisement')
+
+
+# OUTLIERS
+# Plotting studentized residuals
+plot(predict(lm.fit),rstudent(lm.fit),xlab='Fitted values',ylab='Studentized Residuals',main='Studentized Residual Plot for Advertisement')
+
+
+# par(mfrow=c(2,2)) divides plot region in 2 x 2
+par(mfrow=c(2,2)) 
+plot(lm.fit) 
+
 # You can start trying a simple linear regression:
 # Predicting Sales only from TV budget
 # Predicting Sales only from Newspaper budget
