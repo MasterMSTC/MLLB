4f588c5c5df7af5f5be485feec6ddcbe25beaec3
MSTC Signal Processing and Machine Learning for Big Data <master.stc.ssr@gmail.com>
2019-09-25 17:44:10 +0200

    Update WORKING_LinearRegression_Advertising.R

--- WORKING_LinearRegression_Advertising.R
+++ WORKING_LinearRegression_Advertising.R
@@ -3,5 +3,14 @@
 #############################################################
 
 # List objects in this session
+list=ls()
 
+# remove all (memory clean)
+rm(list=ls())
 
+# Set Working directory
+setwd("D:/MSTC_BD/R/Curso_2019_20/LINEAR_REGRESSION/DATA")
+
+#Read the csv file into Advertising
+Advertising=read.csv("Advertising.csv")
+fix(Advertising)
